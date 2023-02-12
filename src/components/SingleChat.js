579c0b63a06806b27df26e50a0b1ebd7b2f0aa9c
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2023-02-12 16:40:33 +0530

    added read By

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -1,3 +1,294 @@
+// import { FormControl } from "@chakra-ui/form-control";
+// import { Input } from "@chakra-ui/input";
+// import { Box, Text } from "@chakra-ui/layout";
+// import "./styles.css";
+// import { IconButton, Spinner, useToast } from "@chakra-ui/react";
+// import { getSender, getSenderFull } from "../config/ChatLogics";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { ArrowBackIcon } from "@chakra-ui/icons";
+// import ProfileModal from "./miscellaneous/ProfileModal";
+// import ScrollableChat from "./ScrollableChat";
+// import Lottie from "react-lottie";
+// import animationData from "../animations/typing.json";
+// import "../App.css"
+// import io from "socket.io-client";
+// import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
+// import { ChatState } from "../Context/ChatProvider";
+// import background from "../animations/background.jpg"
+// const ENDPOINT = "https://lazy-pear-sea-lion-tam.cyclic.app/ "; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+// var socket, selectedChatCompare;
+
+// const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+//   const [messages, setMessages] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [newMessage, setNewMessage] = useState("");
+//   const [socketConnected, setSocketConnected] = useState(false);
+//   const [typing, setTyping] = useState(false);
+//   const [istyping, setIsTyping] = useState(false);
+//   const toast = useToast();
+
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationData,
+//     rendererSettings: {
+//       preserveAspectRatio: "xMidYMid slice",
+//     },
+//   };
+//   const { selectedChat, setSelectedChat, user, notification, setNotification } =
+//     ChatState();
+
+//   const fetchMessages = async () => {
+//     if (!selectedChat) return;
+
+//     try {
+//       const config = {
+//         headers: {
+//           Authorization: `Bearer ${user.token}`,
+//         },
+//       };
+
+//       setLoading(true);
+
+//       const { data } = await axios.get(
+//         `https://lazy-pear-sea-lion-tam.cyclic.app/api/message/${selectedChat._id}`,
+//         config
+//       );
+//       setMessages(data);
+//       setLoading(false);
+
+//       socket.emit("join chat", selectedChat._id);
+//     } catch (error) {
+//       toast({
+//         title: "Error Occured!",
+//         description: "Failed to Load the Messages",
+//         status: "error",
+//         duration: 5000,
+//         isClosable: true,
+//         position: "bottom",
+//       });
+//     }
+//   };
+
+//   const sendMessage = async (event) => {
+//     if (event.key === "Enter" && newMessage) {
+//       socket.emit("stop typing", selectedChat._id);
+//       try {
+//         const config = {
+//           headers: {
+//             "Content-type": "application/json",
+//             Authorization: `Bearer ${user.token}`,
+//           },
+//         };
+//         setNewMessage("");
+//         const { data } = await axios.post(
+//           "https://lazy-pear-sea-lion-tam.cyclic.app/api/message",
+//           {
+//             content: newMessage,
+//             chatId: selectedChat,
+//           },
+//           config
+//         );
+//         socket.emit("new message", data);
+//         setMessages([...messages, data]);
+//       } catch (error) {
+//         toast({
+//           title: "Error Occured!",
+//           description: "Failed to send the Message",
+//           status: "error",
+//           duration: 5000,
+//           isClosable: true,
+//           position: "bottom",
+//         });
+//         console.log(error);
+//       }
+//     }
+//   };
+
+//   useEffect(() => {
+//     socket = io(ENDPOINT);
+//     socket.emit("setup", user);
+//     socket.on("connected", () => setSocketConnected(true));
+//     socket.on("typing", () => setIsTyping(true));
+//     socket.on("stop typing", () => setIsTyping(false));
+
+//     // eslint-disable-next-line
+//   }, []);
+
+//   useEffect(() => {
+//     fetchMessages();
+
+//     selectedChatCompare = selectedChat;
+//     // eslint-disable-next-line
+//   }, [selectedChat]);
+
+//   useEffect(() => {
+//     socket.on("message recieved", (newMessageRecieved) => {
+//       if (
+//         !selectedChatCompare || // if chat is not selected or doesn't match current chat
+//         selectedChatCompare._id !== newMessageRecieved.chat._id
+//       ) {
+//         if (!notification.includes(newMessageRecieved)) {
+//           setNotification([newMessageRecieved, ...notification]);
+//           setFetchAgain(!fetchAgain);
+//         }
+//       } else {
+//         setMessages([...messages, newMessageRecieved]);
+//       }
+//     });
+//   });
+
+//   const typingHandler = (e) => {
+//     setNewMessage(e.target.value);
+
+//     if (!socketConnected) return;
+
+//     if (!typing) {
+//       setTyping(true);
+//       socket.emit("typing", selectedChat._id);
+//     }
+//     let lastTypingTime = new Date().getTime();
+//     var timerLength = 3000;
+//     setTimeout(() => {
+//       var timeNow = new Date().getTime();
+//       var timeDiff = timeNow - lastTypingTime;
+//       if (timeDiff >= timerLength && typing) {
+//         socket.emit("stop typing", selectedChat._id);
+//         setTyping(false);
+//       }
+//     }, timerLength);
+//   };
+
+//   return (
+//     <>
+//       {selectedChat ? (
+//         <>
+//           <Text
+//             fontSize={{ base: "20px", md: "30px" }}
+//             pb={3}
+//             px={2}
+//             w="100%"
+//             fontFamily="Work sans"
+//             d="flex"
+//             justifyContent={{ base: "space-between" }}
+//             alignItems="center"
+//             style={{ display: "flex" }}
+            
+//           >
+//             {/* <IconButton
+//               className="display-small-screen"
+//               d={{ base: "flex", md: "none" }}
+//               icon={<ArrowBackIcon />}
+//               onClick={() => setSelectedChat("")}
+//               style={{ display: "flex" }}
+//             /> */}
+//             {messages &&
+//               (!selectedChat.isGroupChat ? (
+//                 <>
+//                   {/* {getSender(user, selectedChat.users)} */}
+//                   {user._id === selectedChat.users[0]._id ? selectedChat.users[1].name : selectedChat.users[0].name}
+//                   <ProfileModal
+//                     user={getSenderFull(user, selectedChat.users)}
+//                   />
+//                 </>
+//               ) : (
+//                 <>
+//                   {selectedChat.chatName.toUpperCase()}
+//                   <UpdateGroupChatModal
+//                     fetchMessages={fetchMessages}
+//                     fetchAgain={fetchAgain}
+//                     setFetchAgain={setFetchAgain}
+//                   />
+//                 </>
+//               ))}
+//           </Text>
+//           <Box
+//             d="flex"
+//             flexDir="column"
+//             justifyContent="flex-end"
+//             p={3}
+//             w="100%"
+//             h="87.5vh"
+//             borderRadius="lg"
+//             overflowY="scroll"
+//             style={{
+//               display: "flex",
+//               backgroundSize: "cover",
+//               backgroundRepeat: "no-repeat",
+//               backgroundImage:
+//                 "url(" +
+//                 "https://wallpapers.com/images/file/whatsapp-chat-flying-bird-vector-rfw64608p6ous6hs.jpg" +
+//                 ")",
+
+//               // "url(" + "https://wallpaperaccess.com/full/424833.jpg" + ")",
+//               // `url(${background})`
+
+//               // backgroundColor: "#CDCDCD",
+//             }}
+//           >
+//             {loading ? (
+//               <Spinner
+//                 size="xl"
+//                 w={20}
+//                 h={20}
+//                 alignSelf="center"
+//                 margin="auto"
+//               />
+//             ) : (
+//               <div className="messages">
+//                 <ScrollableChat messages={messages} />
+//               </div>
+//             )}
+
+//             <FormControl
+//               onKeyDown={sendMessage}
+//               id="first-name"
+//               isRequired
+//               mt={3}
+//             >
+//               {istyping ? (
+//                 <div>
+//                   <Lottie
+//                     options={defaultOptions}
+//                     // height={50}
+//                     width={70}
+//                     style={{ marginBottom: 15, marginLeft: 0 }}
+//                   />
+//                 </div>
+//               ) : (
+//                 <></>
+//               )}
+//               <Input
+//                 // variant="filled"
+//                 bg="#fff"
+//                 placeholder="Enter a message.."
+//                 value={newMessage}
+//                 onChange={typingHandler}
+//               />
+//             </FormControl>
+//           </Box>
+//         </>
+//       ) : (
+//         // to get socket.io on same page
+//         <Box
+//           d="flex"
+//           alignItems="center"
+//           justifyContent="center"
+//           h="100%"
+//           style={{ display: "flex" }}
+//         >
+//           {/* <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+//             Click on a user to start chatting
+//           </Text> */}
+//         </Box>
+//       )}
+//     </>
+//   );
+// };
+
+// export default SingleChat;
+
 import { FormControl } from "@chakra-ui/form-control";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
@@ -11,16 +302,17 @@ import ProfileModal from "./miscellaneous/ProfileModal";
 import ScrollableChat from "./ScrollableChat";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
-import "../App.css"
+import "../App.css";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-import background from "../animations/background.jpg"
+import background from "../animations/background.jpg";
 const ENDPOINT = "https://lazy-pear-sea-lion-tam.cyclic.app/ "; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const [messages, setMessages] = useState([]);
+  const [readers, setReaders] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState("");
   const [socketConnected, setSocketConnected] = useState(false);
@@ -38,6 +330,10 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   };
   const { selectedChat, setSelectedChat, user, notification, setNotification } =
     ChatState();
+
+  if (selectedChat && !readers.includes(user._id)) {
+    readers.push(user._id);
+  }
 
   const fetchMessages = async () => {
     if (!selectedChat) return;
@@ -87,6 +383,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           {
             content: newMessage,
             chatId: selectedChat,
+            readBy: readers,
           },
           config
         );
@@ -174,7 +471,6 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             justifyContent={{ base: "space-between" }}
             alignItems="center"
             style={{ display: "flex" }}
-            
           >
             {/* <IconButton
               className="display-small-screen"
@@ -187,7 +483,9 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
               (!selectedChat.isGroupChat ? (
                 <>
                   {/* {getSender(user, selectedChat.users)} */}
-                  {user._id === selectedChat.users[0]._id ? selectedChat.users[1].name : selectedChat.users[0].name}
+                  {user._id === selectedChat.users[0]._id
+                    ? selectedChat.users[1].name
+                    : selectedChat.users[0].name}
                   <ProfileModal
                     user={getSenderFull(user, selectedChat.users)}
                   />
